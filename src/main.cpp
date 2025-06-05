512505a05519f5dd679685b12a886c06f910bc1a
Rich Johnson <richjohnson905@gmail.com>
2025-06-05 12:45:20 -0500

    Update to SFML 3.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
+#include <SFML/window.hpp>
+#include <SFML/graphics.hpp>
+#include <SFML/system.hpp>
 #include <iostream>
+#include <cmath>
 /*
       t
  a---------b
@@ -9,12 +11,12 @@
     \   /
       c
  */
-int width = 729; // 3^6
-int height = 729;
+unsigned int width = 729; // 3^6
+unsigned int height = 729;
 
 struct Point {
-    int x;
-    int y;
+    float x;
+    float y;
 };
 
 struct Triangle {
@@ -26,14 +28,15 @@ struct Triangle {
 void getVertex(Point first, Point second, sf::Vertex v[2]);
 void drawTriangle(sf::RenderWindow& window, Triangle triangle);
 
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(width, height), "Simple SFML Example");
+    sf::RenderWindow window(sf::VideoMode({width, height}), "Simple SFML Example");
 
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+
+        while (const std::optional<sf::Event> event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
                 window.close();
         }
         window.clear(sf::Color::Black);
@@ -61,11 +64,11 @@ void drawTriangle(sf::RenderWindow& window, Triangle triangle) {
     sf::Vertex v2;
     sf::Vertex line[] = {v1, v2};
     getVertex(triangle.a, triangle.b, line);
-    window.draw(line, 2, sf::Lines);
+    window.draw(line, 2, sf::PrimitiveType::Lines);
     getVertex(triangle.b, triangle.c, line);
-    window.draw(line, 2, sf::Lines);
+    window.draw(line, 2, sf::PrimitiveType::Lines);
     getVertex(triangle.c, triangle.a, line);
-    window.draw(line, 2, sf::Lines);
+    window.draw(line, 2, sf::PrimitiveType::Lines);
     Triangle t;
     t.a.x = triangle.a.x + (triangle.c.x - triangle.a.x) / 2;
     t.a.y = triangle.a.y - (triangle.c.y - triangle.a.y) / 2;
@@ -93,6 +96,14 @@ void drawTriangle(sf::RenderWindow& window, Triangle triangle) {
 }
 
 void getVertex(Point first, Point second, sf::Vertex v[2]) {
-    v[0] = sf::Vertex(sf::Vector2f(first.x, first.y));
-    v[1] = sf::Vertex(sf::Vector2f(second.x, second.y));
+    sf::Vertex vertex1;
+    vertex1.position = {first.x, first.y};
+    vertex1.color = sf::Color::White;
+    vertex1.texCoords = {0, 0};
+    sf::Vertex vertex2;
+    vertex2.position = {second.x, second.y};
+    vertex2.color = sf::Color::White;
+    vertex2.texCoords = {0, 0};
+    v[0] = vertex1;
+    v[1] = vertex2;
 }
